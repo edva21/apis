981e329bc889253286cc24436f7ffc776ddfe528
Eddy V <edva59012@outlook.com>
2019-03-26 06:32:39 -0600

    get method added

---
+++ do_get_account.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+api_token = 'your_api_token'
+api_url_base = 'https://api.digitalocean.com/v2/'
+
+headers = {'Content-Type': 'application/json',
+           'Authorization': 'Bearer {0}'.format(api_token)}
+
+def get_account_info():
+
+    api_url = '{0}account'.format(api_url_base)
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
